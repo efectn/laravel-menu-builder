0a09b306b7648f72bb6ddcf9deaf2b831ad2317c
Muhammed Efe Çetin <efectn@protonmail.com>
2022-04-30 15:12:26 +0300

    fix scripts.blade.php

--- src/Views/scripts.blade.php
+++ src/Views/scripts.blade.php
@@ -1,31 +1,31 @@
 <script>
-	var menus = {
-		"oneThemeLocationNoMenus" : "",
-		"moveUp" : @lang("menu-builder::move_up"),
-		"moveDown" : @lang("menu-builder::move_down"),
-		"moveToTop" : @lang("menu-builder::move_top"),
-		"moveUnder" : @lang("menu-builder::move_under"),
-		"moveOutFrom" : @lang("menu-builder::move_out_from"),
-		"under" : @lang("menu-builder::under"),
-		"outFrom" : @lang("menu-builder::out_from"),
-		"menuFocus" : @lang("menu-builder::menu_focus"),
-		"subMenuFocus" : @lang("menu-builder::submenu_focus")
-	};
-	var arraydata = [];     
-	var addcustommenur= '{{ route("menus.items.create") }}';
-	var updateitemr= '{{ route("menus.items.update")}}';
-	var generatemenucontrolr= '{{ route("menus.update") }}';
-	var deleteitemmenur= '{{ route("menus.items.delete") }}';
-	var deletemenugr= '{{ route("menus.delete") }}';
-	var createnewmenur= '{{ route("menus.create") }}';
-	var csrftoken="{{ csrf_token() }}";
-	var menuwr = "{{ url()->current() }}";
+    var menus = {
+        "oneThemeLocationNoMenus": "",
+        "moveUp": '{{__("menu-builder::messages.move_up")}}',
+        "moveDown": '{{__("menu-builder::messages.move_down")}}',
+        "moveToTop": '{{__("menu-builder::messages.move_top")}}',
+        "moveUnder": '{{__("menu-builder::messages.move_under")}}',
+        "moveOutFrom": '{{__("menu-builder::messages.move_out_from")}}',
+        "under": '{{__("menu-builder::messages.under")}}',
+        "outFrom": '{{__("menu-builder::messages.out_from")}}',
+        "menuFocus": '{{__("menu-builder::messages.menu_focus")}}',
+        "subMenuFocus": '{{__("menu-builder::messages.submenu_focus")}}'
+    };
+    var arraydata = [];
+    var addcustommenur = '{{ route("menus.items.create") }}';
+    var updateitemr = '{{ route("menus.items.update")}}';
+    var generatemenucontrolr = '{{ route("menus.update") }}';
+    var deleteitemmenur = '{{ route("menus.items.delete") }}';
+    var deletemenugr = '{{ route("menus.delete") }}';
+    var createnewmenur = '{{ route("menus.create") }}';
+    var csrftoken = "{{ csrf_token() }}";
+    var menuwr = "{{ url()->current() }}";
 
-	$.ajaxSetup({
-		headers: {
-			'X-CSRF-TOKEN': csrftoken
-		}
-	});
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': csrftoken
+        }
+    });
 </script>
 <script type="text/javascript" src="{{asset('vendor/menu-builder/scripts.js')}}"></script>
 <script type="text/javascript" src="{{asset('vendor/menu-builder/scripts2.js')}}"></script>
